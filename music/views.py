94f154dc71bd08927a3422c15fd5299740a83483
jakeChal <jacob.chalegoua@gmail.com>
2018-03-01 21:56:21 +0200

    Generate http response with template

--- music/views.py
+++ music/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
@@ -7,8 +6,11 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums' : all_albums }
-    return render(request,'music/index.html',context)
+    return render(request,'music/index.html',{'all_albums' : all_albums })
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html',{'album':album})
