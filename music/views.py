f0d8ab93fc0db3eebc6964745465c9f62b188789
jakeChal <jacob.chalegoua@gmail.com>
2018-03-01 20:24:03 +0200

    Generate http response with template

--- music/views.py
+++ music/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 # take user request and respond with something
 
 def index(request):
     all_albums = Album.objects.all()
-    html=''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.albumTitle + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_albums,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
