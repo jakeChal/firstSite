2ac6cf9fa020f98fbbf3498eaa86a4b9bf12080d
jakeChal <jacob.chalegoua@gmail.com>
2018-03-02 19:37:37 +0200

    Updated detail page. Removed hardcoded URLs.

--- music/urls.py
+++ music/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',views.index, name='index'),
 
     # /music/71/
-    re_path(r'^(?P<album_id>[0-9]+)/$',views.detail)
+    re_path(r'^(?P<album_id>[0-9]+)/$',views.detail, name='detail')
+
 ]
