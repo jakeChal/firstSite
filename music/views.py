a2609b32e17cb3589c70d235a21dcbcbce8fbd77
jakeChal <jacob.chalegoua@gmail.com>
2018-03-02 18:09:51 +0200

    Added Song to admin page. Added function for string repres. of Song

--- music/views.py
+++ music/views.py
@@ -2,15 +2,17 @@ from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
+
 # take user request and respond with something
 
 def index(request):
     all_albums = Album.objects.all()
-    return render(request,'music/index.html',{'all_albums' : all_albums })
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
 
 def detail(request, album_id):
     try:
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
         raise Http404("Album does not exist")
-    return render(request, 'music/detail.html',{'album':album})
+    return render(request, 'music/detail.html', {'album': album})
